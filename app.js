58a9a7b052432104b531dddfcfba48bc73b87b1a
Arnaud Defrancq <arnaud.defrancq@laposte.net>
2022-11-17 22:11:32 +0100

    suppression du carousel

--- app.js
+++ app.js
@@ -13,106 +13,28 @@ links.forEach((link) => {
   });
 });
 
-// Carrousel
-class Carousel {
-  constructor(element, options = {}) {
-    this.element = element;
-    this.options = Object.assign(
-      {},
-      {
-        slideToScroll: 1,
-        slideVisible: 1,
-      },
-      options
-    );
-    let children = [].slice.call(element.children);
-    this.currentSlide = 0;
+// // transition
 
-    this.root = this.createDivWithClass("carousel1");
-    this.panorama = this.createDivWithClass("carousel1__panorama");
+const ratio = 0.1;
 
-    this.root.appendChild(this.panorama);
-    this.element.appendChild(this.root);
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: ratio,
+};
 
-    this.items = children.map((child) => {
-      let item = this.createDivWithClass("carousel__item");
-
-      item.appendChild(child);
-      this.panorama.appendChild(item);
-      return item;
-    });
-    this.setStyle();
-    this.createNavigation();
-  }
-
-  createDivWithClass(className) {
-    let div = document.createElement("div");
-    div.setAttribute("class", className);
-    return div;
-  }
-
-  setStyle() {
-    let ratio = this.items.length / this.options.slideVisible;
-    this.panorama.style.width = ratio * 100 + "%";
-    this.items.forEach(
-      (item) =>
-        (item.style.width = 100 / this.options.slideVisible / ratio + "%")
-    );
-  }
-
-  createNavigation() {
-    let nextBtn = this.createDivWithClass("carousel__next");
-    let prevBtn = this.createDivWithClass("carousel__prev");
-    this.root.appendChild(nextBtn);
-    this.root.appendChild(prevBtn);
-    nextBtn.addEventListener("click", this.next.bind(this));
-    prevBtn.addEventListener("click", this.prev.bind(this));
-  }
-
-  next() {
-    this.goToItem(this.currentSlide + this.options.slideToScroll);
-  }
-
-  prev() {
-    this.goToItem(this.currentSlide - this.options.slideToScroll);
-  }
-
-  goToItem(index) {
-    if (index < 0) {
-      index = this.items.length - this.options.slideVisible;
-    } else if (
-      index >= this.items.length ||
-      this.items[this.currentSlide + this.options.slideVisible] === undefined
-    ) {
-      index = 0;
+const handlIntersect = (entries, observer) => {
+  entries.forEach((entrie) => {
+    if (entrie.intersectionRatio > ratio) {
+      entrie.target.classList.add("reveal-visible");
+      observer.unobserve(entrie.target);
     }
-    let translateX = (index * -100) / this.items.length;
-    this.panorama.style.transform = "translate3d(" + translateX + "%, 0, 0)";
-    this.panorama.style.transition = "0.4s ease";
-    this.currentSlide = index;
-  }
-}
+    // console.log(entrie.intersectionRatio);
+  });
+  console.log("handle");
+};
 
-let widthPage = window.innerWidth;
-console.log(widthPage);
-
-new Carousel(document.querySelector("#carousel1"), {
-  slideVisible: 3,
+const observer = new IntersectionObserver(handlIntersect, options);
+document.querySelectorAll(".reveal").forEach((r) => {
+  observer.observe(r);
 });
-
-if (widthPage <= 834) {
-  document.querySelector("#carousel1").classList.add("slider2");
-  document.querySelector("#carousel2").classList.remove("slider2");
-  new Carousel(document.querySelector("#carousel2"), {
-    slideVisible: 2,
-  });
-}
-
-if (widthPage <= 500) {
-  document.querySelector("#carousel1").classList.add("slider2");
-  document.querySelector("#carousel2").classList.add("slider2");
-  document.querySelector("#carousel3").classList.remove("slider2");
-  new Carousel(document.querySelector("#carousel3"), {
-    slideVisible: 1,
-  });
-}
